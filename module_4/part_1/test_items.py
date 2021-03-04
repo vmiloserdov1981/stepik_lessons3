1c70b30f7f5e57aad93fa0c1c81fd817c6d47e4c
vmiloserdov1981 <vmiloserdov1981@yandex.ru>
2021-03-04 09:49:20 +0300

    ДЗ к 4-му модулю

---
+++ module_4/part_1/test_items.py
@@ -0,0 +1,17 @@
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+class TestProductPage:
+
+    def test_button_add_to_basket_is_visible(self, browser):
+#       Тест проверяет, что страница товара содержит кнопку "добавить в корзину"
+
+        # Открываем страницу товара
+        browser.get(link)
+
+        # Пауза 5 сек после открытия страницы, для визуальной проверки языка открытой страницы
+        time.sleep(5)
+
+        # Проверяем наличие кнопки добавления товара в корзину
+        assert browser.find_element_by_css_selector("#add_to_basket_form > button")
---
+++ module_4/part_2/code_sample.py
@@ -0,0 +1,45 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
+import com.thoughtworks.selenium.Selenium
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.WebDriver
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium
+import static org.junit.Assert.*
+import java.util.regex.Pattern
+import static org.apache.commons.lang3.StringUtils.join
+
+WebUI.openBrowser('https://www.google.com/')
+def driver = DriverFactory.getWebDriver()
+String baseUrl = "https://www.google.com/"
+selenium = new WebDriverBackedSelenium(driver, baseUrl)
+selenium.click("link=Oscar")
+selenium.click("id=login_link")
+selenium.click("id=id_registration-email")
+selenium.type("id=id_registration-email", "aasmolov_8@yandex.ru")
+selenium.click("id=id_registration-password1")
+selenium.type("id=id_registration-password1", "1109026$$$")
+selenium.click("id=id_registration-password2")
+selenium.type("id=id_registration-password2", "1109026$$$")
+selenium.click("name=registration_submit")
+// selenium.cont("//*[@id=\"messages\"]/div/div", "Спасибо за регистрацию!")
+selenium.click("id=logout_link")
