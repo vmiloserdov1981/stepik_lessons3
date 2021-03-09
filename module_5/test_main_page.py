6e735ba6762af71e94082787daa7c1c944425b15
vmiloserdov1981 <vmiloserdov1981@yandex.ru>
2021-03-09 16:42:07 +0300

    Проверяет есть ли ссылка на логин и если нет, то выдает более осмысленное сообщение

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -6,4 +6,5 @@ class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
         page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()  # открываем страницу
-        page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+        # page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+        page.should_be_login_link() # Проверить, что есть ссылка, которая ведет на логин
