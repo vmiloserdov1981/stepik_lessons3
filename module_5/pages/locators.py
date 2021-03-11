f1d2b346e5a696548ddb855f83911a7a2307a074
vmiloserdov1981 <vmiloserdov1981@yandex.ru>
2021-03-11 16:15:08 +0300

    Добавил локаторы для класса ProductPageLocators (стр. Продукт добавляем в корзину)

--- module_5/pages/locators.py
+++ module_5/pages/locators.py
@@ -7,3 +7,10 @@ class LoginPageLocators():
     login_url = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     login_form = (By.CSS_SELECTOR, "#login_form > h2")
     register_form = (By.CSS_SELECTOR, "#register_form > h2")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, ".add-to-basket button")
+    ADDED_PRODUCT_TEXT = (By.CSS_SELECTOR, ".alert-success:nth-child(1) strong")
+    BASKET_PRICE_TEXT = (By.CSS_SELECTOR, ".alertinner p strong")
+    PRODUCT_NAME_TEXT = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE_TEXT = (By.CSS_SELECTOR, ".product_main .price_color")
