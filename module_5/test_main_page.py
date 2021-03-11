a73c5f9444e6f9b2bc4c228c46dd95eb058f103d
vmiloserdov1981 <vmiloserdov1981@yandex.ru>
2021-03-11 11:15:12 +0300

    собственно, добавил шаги проверок: проверка url, стр. регистрации и стр.логина

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,10 +1,22 @@
 from pages.main_page import MainPage
+import time
+from pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
+
 class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()  # открываем страницу
-        # page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-        page.should_be_login_link() # Проверить, что есть ссылка, которая ведет на логин
+        # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        mainPage = MainPage(browser, link)
+        mainPage.open()  # открываем страницу
+        time.sleep(2)
+        mainPage.should_be_login_link()  # Проверить, что есть ссылка, которая ведет на логин
+        time.sleep(2)
+        mainPage.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+        time.sleep(2)
+
+        loginPage = LoginPage(browser, browser.current_url)
+        loginPage.should_be_login_url()
+        loginPage.should_be_login_form()
+        loginPage.should_be_register_form()
