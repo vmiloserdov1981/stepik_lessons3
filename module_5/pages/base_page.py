2a58d46112717b55e79846c9e509c337bd94c94c
vmiloserdov1981 <vmiloserdov1981@yandex.ru>
2021-03-16 16:18:24 +0300

    вынес сюда методы из main_page, go_to_login_page и should_be_login_link

--- module_5/pages/base_page.py
+++ module_5/pages/base_page.py
@@ -1,11 +1,24 @@
-from selenium.common.exceptions import NoSuchElementException
 import math
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout) # В конструктор BasePage добавим команду для неявного ожидания со значением по умолчанию в 10
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
 
     def open(self):
         self.browser.get(self.url)
@@ -15,6 +28,24 @@ class BasePage():
             self.browser.find_element(how, what)
         except (NoSuchElementException):
             return False
+        return True
+
+    # проверяет, что элемент не появляется на странице в течение заданного времени
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    #проверить, что какой-то элемент исчезает, то следует воспользоваться явным ожиданием
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
         return True
 
     # Посчитать результат математического выражения и ввести ответ
